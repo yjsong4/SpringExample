0b844b83db68deac6e0ca6e531acfe6f1f8bb7cc
megaitsong <nana@naver.com>
2024-02-22 18:46:21 +0900

    [송여진] - insert ex

--- src/main/java/com/syj/spring/ex/mybatis/repository/ReviewRepository.java
+++ src/main/java/com/syj/spring/ex/mybatis/repository/ReviewRepository.java
@@ -10,5 +10,14 @@ public interface ReviewRepository {
 	
 	// 전달 받은 id와 일치하는 리뷰 행 조회
 	public Review selectReview(@Param("id") int id);
-
+	
+	public int insertReview(
+			@Param("storeId") int storeId
+			, @Param("menu") String menu
+			, @Param("userName") String userName
+			, @Param("point") double point
+			, @Param("review") String review);
+	
+	public int insertReviewByObject(Review review);
+	
 }
